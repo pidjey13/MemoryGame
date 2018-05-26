7b4f411c02aa3c59fe94c3243be9b32f58e6ecb2
PJ <pidjey@hotmail.it>
2018-05-26 14:34:11 +0200

    Added timer to HTML and reset moves counter with restart game btn click

--- js/app_refactored.js
+++ js/app_refactored.js
@@ -2,16 +2,23 @@
 // ==> used for a single evt listener
 const deck = document.querySelector(".deck");
 
+// restart button
+const restart = document.querySelector(".restart");
+
+// array-list that holds all of the cards and convert the object into Array
+var cards = document.querySelectorAll(".deck .card");
+let cardsArray = Array.from(cards);
+
 // array of cards clicked used to check a match
 let clicked_cards = [];
 
 // moves counter incremented after the 2nd click
 let moves = 0;
 
+
 // Function that checks if icon-classes have a match.
 // It will be called only after a 2nd click.
 function check_suit() {
-
 	/* 
 	*  Statement that check if there is a match on the cards' suit,
 	*  and if this is the case, adds "match" CSS class,
@@ -33,7 +40,7 @@ function check_suit() {
 	} else {
 
 		setTimeout(() => {
-			
+
 			clicked_cards[0].classList.remove("open", "show");
 			clicked_cards[1].classList.remove("open", "show");
 
@@ -48,21 +55,30 @@ function check_suit() {
 
 		clicked_cards[0].classList.remove("disabled");
 		clicked_cards[1].classList.remove("disabled");
-	
+
 	}
 }
-
 
 
 // Add an evt listener on the deck --- THANKS TO MIKE WALES FOR THE TIP
 deck.addEventListener('click', el => {
 
+	console.log(el.target.classList)
+	console.log(el.target)
+	console.log(el.target.parentNode.classList)
+
 	// Check if the click has been made on a card and then stores it into
 	// a variable which is going to be pushed later in order to be checked
-  if (el.target.classList == "card") {
+	if (el.target.classList == "card" || el.target.parentNode.classList == "card") {
 
-		let card = el.target;
-		
+		let card;
+
+		if (el.target.classList == "card"){
+			card = el.target;
+		} else {
+			card = el.target.parentNode;
+		}
+
 		// Now I add this CSS class in order to not having the
 		// 2nd-click-on-the-same-card bug
 		card.classList.add("disabled", "open", "show");
@@ -79,144 +95,27 @@ deck.addEventListener('click', el => {
 			check_suit();
 
 			document.querySelector(".moves").innerHTML = moves += 1;
-
 		}
 
 	}
-	
+
 });
-
-
-
-
-/* --------------------------------------------- */
-
-
-
-
-// // counter for clicks
-// let counter = 0;
-
-// // array of cards' suit
-// let checkSuit = [];
-
-// // array of cards clicked
-// var clicked_cards = [];
-
-// // evt listener on the deck
-// deck.addEventListener('click', el => {
-
-// 	//console.log(deck);
-// 	//console.log("click");
-// 	//console.log(el);
-// 	//console.log(el.target);
-// 	//console.log(el.target.classList);
-
-
-// 	/* check if a - ONLY - covered card has been clicked.
-// 	* will not work if the click happens on a already flipped one
-// 	* because of a value == to "card + otherClass"
-// 	*/
-
-// 	let card_value = el.target.classList.value;
-// 	let card_suit = el.target.childNodes[1].classList[1];
-
-// 	if (card_value == "card") {
-// 		// console.log("card");
-// 		// console.log(card_suit);
-
-// 		// I count the 1st click
-// 		counter += 1;
-// 		// console.log(counter);
-
-// 		let card = el.target;
-		
-// 		card.classList.add("open", "show");
-// 		// console.log(card);
-
-// 		clicked_cards.push(card);
-// 		// console.log(clicked_cards);
-// 		//console.log("clicked_cards length", clicked_cards.length);
-
-// 		// I push the card suit to check it later if match or not
-// 		checkSuit.push(card_suit);
-// 		// console.log(checkSuit);
-
-// 		// now I check if a 2nd click has been made
-// 		if (counter > 1){
-// 			// console.log(checkSuit);
-// 			// console.log(checkSuit[1]);
-
-// 			// reset the counter
-// 			counter = 0;
-// 			//console.log("counter resettato");
-// 			//console.log("checkSuit.length", checkSuit.length);
-// 			// console.log(typeof card);
-
-// 			// and now the check on the card suit
-// 			if (checkSuit[0] == checkSuit[1]) {
-// 				// console.log("match");
-
-// 				clicked_cards[0].classList.add("match");
-// 				clicked_cards[1].classList.add("match");
-// 				// card.classList.add("match");
-// 				//console.log(clicked_cards);
-
-// 			} else {
-
-// 				// console.log(card);
-
-// 				setTimeout( () =>  {
-// 					//console.log("clicked_cards ", clicked_cards);
-// 					clicked_cards[0].classList.remove("open", "show");
-// 					clicked_cards[1].classList.remove("open", "show");
-// 					//card.classList.remove("open", "show");
-// 				}, 200);
-
-// 			}
-
-// 			// reset of my array of cards' suit
-// 			checkSuit = [];
-
-// 			// time to reset my clicked cards array.
-// 			// without it, it won't work because of the other setTimeou into else
-// 			setTimeout(() => {
-// 				clicked_cards = [];
-// 			}, 700);
-// 			//clicked_cards = [];
-
-// 		}
-
-// 	} else {
-
-// 		// console.log("NOPE");
-
-// 	}
-
-// })
-
-
-
-
-/* --------------------------------------------- */
-
-
-
-
-// restart button
-const restart = document.querySelector(".restart");
-
-// array-list that holds all of the cards and convert the object into Array
-var cards = document.querySelectorAll(".deck .card");
-let cardsArray = Array.from(cards);
 
 
 // START THE GAME
 window.onload = startGame();
 
-// restart button CLICK LISTENER
+/* 
+*  Restart button CLICK LISTENER
+*  After the user click on the restart btn, startGame will be fired.
+*  startGame() will shuffle the cards.
+*  moves counter back to 0 and uploaded the DOM.
+*/
 restart.addEventListener('click', () => {
 	startGame();
+
+	moves = 0;
+	document.querySelector(".moves").innerHTML = moves;
 });
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -234,15 +133,17 @@ function shuffle(array) {
 	return array;
 }
 
+// function that will shuffle the deck of cards
+// by shuffling cards' suits
 function startGame() {
 	var shuffledDeck = shuffle(cardsArray);
-	
+
 	let outer = "";
 	var reconstructHTML = () => {
 		shuffledDeck.forEach(card => {
 			outer += card.outerHTML;
 		});
-	
+
 		deck.innerHTML = outer;
 	}
 
